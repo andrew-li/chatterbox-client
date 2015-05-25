e8603464f0e3a0bc68d82d54f83c62ab62204575
andrew-li <ali91007@gmail.com>
2015-05-25 16:51:22 -0700

    changed to pseudoclassical style

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,55 @@
 // YOUR CODE HERE:
 
-
-var makeRoom = function(roomData) {
-  var room = '<div>'
-    + JSON.stringify(roomData.roomname)
-    + '</div>';
-
-  return room;
+var App = function() {
+  this.roomList = {};
 };
 
+App.prototype.replaceEscapeCharacters = function(str) {
+  var tempStr;
+  if(str === undefined)
+    tempStr = "undefined";
+  else if(str === null)
+    tempStr = "null";
+  else
+   tempStr = JSON.stringify(str);
 
-var handleData = function(data) {
+  return tempStr.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\"/g, '&quot;')
+    .replace(/\'/g, '&#39;');
+};
+
+App.prototype.makeRooms = function() {
+
+  // var roomname;
+  // if(roomData.roomname === undefined)
+  //   roomname = "undefined";
+  // else if(roomData.roomname === null)
+  //   roomname = "null";
+  // else
+  //  roomname = replaceEscapeCharacters(JSON.stringify(roomData.roomname));
+
+  // var room = '<div>'
+  //   + roomname
+  //   + JSON.stringify(roomData.text)
+  //   + '</div>';
+
+  // return room;
+
+  var room;
+  for(var roomName in this.roomList)
+  {
+    room = '<div>'
+    + roomName
+    + '</div>';
+
+    $('#main').append(room);
+  }
+
+};
+
+App.prototype.handleData = function(data) {
   console.log(data);
 
   //var results = data.results;
@@ -21,23 +60,40 @@ var handleData = function(data) {
   //   console.log(results[i].roomname);
   // }
 
-  var sortedResults = _.sortBy(data.results, function(item) {
-    return item.roomname;
-  });
+  // var sortedResults = _.sortBy(data.results, function(item) {
+  //   return item.roomname;
+  // });
 
-  console.log(sortedResults);
+  // console.log(sortedResults);
 
 
-  for(var i = 0; i < sortedResults.length; i++)
-  {
-    $('#main').append(makeRoom(sortedResults[i]));
-  }
+  // for(var i = 0; i < sortedResults.length; i++)
+  // {
+  //   $('#main').append(makeRoom(sortedResults[i]));
+  // }
 
+
+  for (var i = 0; i < data.results.length; i++) {
+    var stringifiedRoomName = this.replaceEscapeCharacters(data.results[i].roomname);
+    var stringifiedUserName = this.replaceEscapeCharacters(data.results[i].username);
+    var stringifiedText = this.replaceEscapeCharacters(data.results[i].text);
+
+    if (data.results[i].hasOwnProperty(stringifiedRoomName) === false) {
+      this.roomList[stringifiedRoomName] = [];
+    }
+    this.roomList[stringifiedRoomName].push({"username" : stringifiedUserName,
+                                        "text" : stringifiedText});
+  };
+
+  console.log(this.roomList);
+
+  this.makeRooms();
 
 };
 
-var getMessages = function() {
+App.prototype.getMessages = function() {
 
+  var oldThis = this;
   $.ajax({
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -46,7 +102,7 @@ var getMessages = function() {
     success: function (data) {
       console.log('chatterbox: Message received');
 
-      handleData(data);
+      oldThis.handleData(data);
 
     },
     error: function () {
@@ -57,9 +113,17 @@ var getMessages = function() {
 
 };
 
-
-var init = function() {
-  getMessages();
+App.prototype.init = function() {
+  this.getMessages();
 };
 
-init();
+var app = new App();
+
+app.init();
+
+console.log(app);
+
+
+
+
+
